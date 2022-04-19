fe930b7743d6aafc5c0e70d12699eba670398ce2
yosse95ai <karateyosi@gmail.com>
2022-04-19 17:39:38 +0900

    Add: 初期設定完了

---
+++ src/components/Sample.tsx
@@ -0,0 +1,11 @@
+const Sample: React.FC = () => {
+  return (
+    <div>
+      <button className="button" onClick={() => console.log('button')}>
+        ボタン
+      </button>
+    </div>
+  );
+};
+
+export default Sample;
